f619d23f07afe984eaa3c0229055cd15347c4ae3
JeffreyLiu <17771770679@163.com>
2018-11-12 01:01:49 +0800

    create

---
+++ src/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <input type="text" @keyup.enter="autoSubmit"  v-model="inputValue">
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <todo-item 
+        v-for="(item,index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="deleteItem"
+      >
+      <!-- 监听到子组件派发的delete事件，之后执行deleteItem方法 -->
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  components: {
+    'todo-item':TodoItem
+  },
+
+  data() {
+    return {
+      inputValue: "",
+      list: ['a','b']
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputValue);
+      this.inputValue = ''
+    },
+    autoSubmit() {
+      this.list.push(this.inputValue);
+      this.inputValue = ''
+    },
+    deleteItem(index) {
+      this.list.splice(index,1)
+    },
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <li @click="handleDelete">{{content}}</li>
+</template>
+
+
+<script>
+export default {
+    props:['content','index'],
+    //子组件接收到click事件，派发一个delete事件供父组件监听
+    methods:{
+        handleDelete(){
+            this.$emit('delete',this.index)
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
